2f61172fd20cfb9375e20fed3f64cd1f71f4c486
Kivithedog <victory27@nycstudents.net>
2024-11-19 09:21:03 -0500

    i will eat what i want

---
+++ test gpt.py
@@ -0,0 +1,16 @@
+import urllib.parse
+
+# Function to get Google search URL
+def generate_google_link(song_name):
+    # URL encode the song name to ensure it works in a URL
+    query = urllib.parse.quote(song_name)
+    return f"https://www.google.com/search?q={query}+download"
+
+# Ask user for the song name
+song_name = input("Enter the name of the song: ")
+
+# Generate the Google search link
+google_link = generate_google_link(song_name)
+
+# Output the link
+print(f"Here is a Google search link to download the song: {google_link}")
